984af714f68ccc189a3ab559ff0c7fb9432a6f46
Nikita Madorsky <madorskii@mail.ru>
2021-02-22 21:29:45 +0300

    Настроил проект для публички. Проверка prod_settings 2

--- white_orhideya/prod_settings.py
+++ white_orhideya/prod_settings.py
@@ -5,7 +5,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(00!&lwg@&2na1^$_dbrksnfr5h31_(vhw)q)2ekc(*qpy,zm?'
+SECRET_KEY = '(11!&dhthrycbs625hj92&^he52p1_(vhw)q)nmew*32l,pr?'
 
 
 DEBUG = False
